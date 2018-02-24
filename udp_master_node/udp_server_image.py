6ceac8bec950f4fb6fc4f5709f9db95d3ebe4375
bhve0693 <bhve0693@colorado.edu>
2018-02-24 23:38:39 +0000

    Test Image check

--- udp_master_node/udp_server_image.py
+++ udp_master_node/udp_server_image.py
@@ -6,12 +6,15 @@ UDP_Port = 5005
 print ("Server Started")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_Port))
+filesize = int(sock.recv(1024))
+print ("File Size \n",filesize)
 with open('received_file','wb') as f:
-	while True:
+	while (filesize>0):
 		print ("file opened")
 		data = sock.recv(1024)
-		if not data:
-			break
+		filesize -= 1024
+		#if not data:
+		#	break
 		f.write(data)
 f.close()
 print ("Successfully get the file")
