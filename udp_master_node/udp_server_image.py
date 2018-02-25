09525c077bcd8e942af95346e8614dd01893712f
bhve0693 <bhve0693@colorado.edu>
2018-02-25 06:13:59 +0000

    Master Node Update

--- udp_master_node/udp_server_image.py
+++ udp_master_node/udp_server_image.py
@@ -2,17 +2,20 @@ import socket
 import os
 
 UDP_IP = "192.168.111.101"
-UDP_Port = 5005
+UDP_Port = 5010
 print ("Server Started")
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 sock.bind((UDP_IP, UDP_Port))
 #filesize = int(sock.recv(1024))
 #print ("File Size \n",filesize)
-#i=0
-for i in range(3):
+i=0
+data = sock.recv(1024)
+if data == "M":
+#for i in range(3):
 	filesize= int(sock.recv(1024))
 	print ("File Size\n",filesize)
-	filename = 'received_file_%d.jpg'%(i,)
+	filename = 'u_received_file_%d.jpg'%(i,)
 	with open(filename,'wb') as f:
 		while (filesize>0):
 			print ("file opened")
