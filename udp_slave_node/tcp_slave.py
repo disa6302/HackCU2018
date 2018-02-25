d92e342be115ca4e1ca116cbfae6527779d66ae2
SridharPavithrapu <srpa3332@colorado.edu>
2018-02-25 18:38:28 +0000

    Finl changes for Remote

--- udp_slave_node/tcp_slave.py
+++ udp_slave_node/tcp_slave.py
@@ -1,9 +1,12 @@
 import socket
 import time
 import os
+import sys
+import struct
 
-UDP_MASTER_IP = "192.168.111.101"
-UDP_MASTER_PORT = 5010
+
+TCP_MASTER_IP = "192.168.111.100"
+TCP_MASTER_PORT = 5010
 
 UDP_IP = "192.168.111.100"
 UDP_PORT = 5006
@@ -11,13 +14,13 @@ UDP_PORT = 5006
 UDP_IP_RPI = "192.168.111.99"
 UDP_PORT_RPI = 5009
 
-print "UDP target IP:", UDP_MASTER_IP
-print "UDP target port:", UDP_MASTER_PORT
-print "UDP receive IP:", UDP_IP
-print "UDP receive port:", UDP_PORT
 
-sock_master = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock_master = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # UDP
 sock_master.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock_master.bind((TCP_MASTER_IP, TCP_MASTER_PORT))
+sock_master.listen(1)
+connection,addr = sock_master.accept()
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -26,9 +29,10 @@ sock.bind((UDP_IP, UDP_PORT))
 sock_slave = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 sock_slave.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-for i in range (0,120):
+for i in range (0,5):
 	sock_slave.sendto("R", (UDP_IP_RPI, UDP_PORT_RPI))
-	print "Sending receiving ack"
+	print "Sent char"
+
 
 	filesize= int(sock.recv(1024))
 	print ("File Size\n",filesize)
@@ -40,24 +44,36 @@ for i in range (0,120):
 					f.write(data)
 	filesize = 0
 	f.close()	
-	print ("Successfully got the file")
-
-	sock_master.sendto("M", (UDP_MASTER_IP, UDP_MASTER_PORT))
+	print ("Successfully get the file")
+	
+	#character = "M"
+	#connection.send(character.encode())
 
 	st = os.stat(filename)
-	sock_master.sendto(str(st.st_size), (UDP_MASTER_IP, UDP_MASTER_PORT))
-
+	size = st.st_size
+	#sep = ' '
+	#val = str(size) + sep # sep = ' ' or sep = `\n`
+	#print val
+	connection.send(str(size))
+	print "sent size"
+	c = connection.recv(1024)
+	print "recv ack"
+	#print (struct.pack('<L', size))
+	#connection.send(struct.pack('<L', size))
+	#connection.flush()
+	time.sleep(0.005)
 	file_master = open(filename,'rb')
 	l = file_master.read(1024)
 	while (l):
-			sock_master.sendto(l, (UDP_MASTER_IP, UDP_MASTER_PORT))
+			connection.send(l)
+			#print('Sent',repr(l))
 			l = file_master.read(1024)
 	file_master.close()
 	print("Sent image\n",i)
 
 print('Done sending')
 	
-sock_master.close()
+connection.close()
 sock.close()
 
 
