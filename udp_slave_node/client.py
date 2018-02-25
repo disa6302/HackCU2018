26b8ebdbaf868b4bc56e1232fed900ee1846edf6
SridharPavithrapu <srpa3332@colorado.edu>
2018-02-25 03:05:13 +0000

    Adding client code

--- udp_slave_node/client.py
+++ udp_slave_node/client.py
@@ -1,6 +1,7 @@
 # server.py
 
 import socket
+import os
 
 UDP_IP = "192.168.111.101"
 UDP_PORT = 5005
@@ -12,14 +13,19 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
 print 'Server listening....'
 
-filename='/data/pastaveg_640x480.jpg'
-f = open(filename,'rb')
-l = f.read(1024)
-while (l):
-    sock.sendto(l, (UDP_IP, UDP_PORT))
-    print('Sent ',repr(l))
-    l = f.read(1024)
-f.close()
+filename='/home/devuser/hackcu/HackCU2018/udp_slave_node/sri.jpg'
+st = os.stat(filename)
+
+for i in range (0,3):
+	sock.sendto(str(st.st_size), (UDP_IP, UDP_PORT))
+
+	f = open(filename,'rb')
+	l = f.read(1024)
+	while (l):
+		sock.sendto(l, (UDP_IP, UDP_PORT))
+    		print('Sent ',repr(l))
+    		l = f.read(1024)
+	f.close()
 
 print('Done sending')
 sock.close()
